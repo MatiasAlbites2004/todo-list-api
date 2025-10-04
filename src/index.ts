a39e068bef4fb701c00bbb9ce70f05e3076f0789
MatiasAlbites2004 <matiasalbites1512@gmail.com>
2025-10-04 03:26:56 -0500

    feat: integration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express from "express";
+import cors from "cors";
 import todoRoutes from "./routes/todo.routes";
 import authRoutes from "./routes/auth.routes";
 
 const app = express();
 const PORT = 3000;
 
+
+app.use(cors({
+  origin: "http://localhost:5173",
+}));
+
 app.use(express.json());
+
 app.use("/todos", todoRoutes);
 app.use("/auth", authRoutes);
 
